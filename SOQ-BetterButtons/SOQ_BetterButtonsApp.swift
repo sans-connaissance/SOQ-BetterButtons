64b6fba333ea043d1ef437c0995c7cf114f2fbd4
sans-connaissance <davemalicke@gmail.com>
2021-11-27 13:41:51 -0500

    Initial Commit

---
+++ SOQ-BetterButtons/SOQ_BetterButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOQ_BetterButtonsApp.swift
+//  SOQ-BetterButtons
+//
+//  Created by David Malicke on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SOQ_BetterButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
